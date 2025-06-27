fa6e60dc8999fdae3652e362b5681f674bb1e482
Jahnavi <jahnavigrandhi2005@gmail.com>
2025-06-27 21:09:13 +0530

    enhanced responsiveness

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -52,7 +52,7 @@ const Hero = () => {
                             <p>Architecting innovative solutions with Full Stack expertise.</p>
                             <motion.div className={`${styles['btn-container']} flexStart`}>
                                 <button className="button" style={{ marginRight: '2rem' }}>
-                                    <a href='https://drive.google.com/file/d/1sk2ruwpWB7oUgFu8JK9gwTrlVDkbXcvb/view?usp=drivesdk' target='_blank' rel="noreferrer">
+                                    <a href='https://drive.google.com/file/d/1soj1agIsNZRkletUp3IRhTlln5zeNvuq/view?usp=drivesdk' target='_blank' rel="noreferrer">
                                         Download CV
                                     </a>
                                 </button>   
