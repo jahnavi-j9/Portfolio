3e03136d2c86c4dac7b7e15d55009a1fd082080b
Jahnavi <your-email@example.com>
2025-07-04 09:37:07 +0530

    aligned

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -52,7 +52,7 @@ const Hero = () => {
                             <p>Architecting innovative solutions with Full Stack expertise.</p>
                             <motion.div className={`${styles['btn-container']} flexStart`}>
                                 <button className="button" style={{ marginRight: '2rem' }}>
-                                    <a href='https://drive.google.com/file/d/1ugAg0S7hj2mzvDviAvjFF3_ikH05NrqR/view?usp=drivesdk' target='_blank' rel="noreferrer">
+                                    <a href='https://drive.google.com/file/d/1uvEe6oRb-mLeyp7M8A81S4a8SXOwxXMU/view?usp=drivesdk' target='_blank' rel="noreferrer">
                                         Download CV
                                     </a>
                                 </button>   
