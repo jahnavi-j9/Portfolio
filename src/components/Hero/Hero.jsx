6efb9c710287f771185766397e940bc3a76564d6
jahnavi-j9 <jahnavigrandhi2005@gmail.com>
2025-07-08 21:16:05 +0530

    updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -52,7 +52,7 @@ const Hero = () => {
                             <p>Architecting innovative solutions with Full Stack expertise.</p>
                             <motion.div className={`${styles['btn-container']} flexStart`}>
                                 <button className="button" style={{ marginRight: '2rem' }}>
-                                    <a href='https://drive.google.com/file/d/1uwy3gDmPyW11RSbx1q-3s3KhMtFeQ2Zd/view?usp=drivesdk' target='_blank' rel="noreferrer">
+                                    <a href='https://drive.google.com/file/d/1vkdc8tid0wTvNB8AOOSsRhyz0viPp3BQ/view?usp=drivesdk' target='_blank' rel="noreferrer">
                                         Download CV
                                     </a>
                                 </button>   
